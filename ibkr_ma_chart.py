08ece2c2da342f1712a235a263a793f8b2f44598
Mike Pinna <36490042+Pinna3@users.noreply.github.com>
2021-01-18 11:50:53 -0500

    Update ibkr_ma_chart.py
    added list of securities to cycle through and for loop to do so.

--- ibkr_ma_chart.py
+++ ibkr_ma_chart.py
@@ -11,8 +11,18 @@ ib.connect('127.0.0.1', 4001, clientId=1)
 # # use this for TWS Workstation
 # ib.connect('127.0.0.1', 7497, clientId=1)
 
-# create a contract for the security being charted
+# list of securities to cycle through
+securities = [
+    ['VFF', 'NASDAQ'],
+    ['GDX', 'ARCA'],
+    ['NEM', 'ARCA'],
+    ['GOLD', 'ARCA'],
+    ['MSOS', 'ARCA'],
+    ['OIH', 'ARCA']
+]
+
 def ma_chart(tickerCapString, primeExchangeCapString, maLength, numDailyTicks):
+    # create a contract for the security being charted
     stock = Stock(tickerCapString, 'SMART', 'USD', primaryExchange = primeExchangeCapString)
     bars = ib.reqHistoricalData(
         stock, endDateTime='', durationStr='365 D', #365days max
@@ -20,13 +30,13 @@ def ma_chart(tickerCapString, primeExchangeCapString, maLength, numDailyTicks):
     # convert to indexable dictionary:
     tree = util.tree(bars)
 
-# create a list of closing prices from pulled data to calculate accompanying...
-# ... moving average plot points
+    # create a list of closing prices from pulled data to calculate accompanying...
+    # ... moving average plot points
     closing_prices = []
     for day in range(len(tree)):
         closing_prices.append((tree[day]['BarData']['close']))
 
-# create accompanying moving average plot points
+    # create accompanying moving average plot points
     ma_length = maLength
     i = 0
     averages = []
@@ -35,19 +45,22 @@ def ma_chart(tickerCapString, primeExchangeCapString, maLength, numDailyTicks):
         window_average = round(sum(this_window) / ma_length, 2)
         averages.append(window_average)
         i += 1
-
-    # plot candlestick stock chart against MA line
-    util.barplot(bars[-90:], upColor='green', downColor='red')
-    plt.plot(range(len(averages[-numDailyTicks:])), averages[-numDailyTicks:])
     # if security is above MA its rich, if security is below MA its cheap
     if closing_prices[len(closing_prices)-1] < averages[len(averages)-1]:
+        # plot candlestick stock chart against MA line
+        util.barplot(bars[-90:], upColor='green', downColor='red')
+        plt.plot(range(len(averages[-numDailyTicks:])), averages[-numDailyTicks:])
         plt.title(f'''
     {tickerCapString} below the {maLength}SMA''')
     else:
-        plt.title(f'''
-    {tickerCapString} above the {maLength}SMA''')
+        print(
+f'''{tickerCapString} out of range.''')
     plt.show()
     # plt.savefig('test_chart.png')
 
-ma_chart('VFF', 'NASDAQ', 9, 90)
+# cycle through list of securities
+for ticker in range(len(securities)):
+    ma_chart(securities[ticker][0], securities[ticker][1], 9, 90)
+
+
 
